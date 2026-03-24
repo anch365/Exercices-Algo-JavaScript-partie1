5f65c892032160ec115b2df1031d8c2c36bd89dd
anch365 <mchindaanchoura@gmail.com>
2026-03-24 10:05:50 +0100

    Exercice 1: Affichage simple

---
+++ main.js
@@ -0,0 +1,7 @@
+//////// EXERCICE 1: AFFICHAGE SIMPLE //////////
+
+let words = "Hello, world !";
+console.log(words);
+
+let words1 = "Bienvenue dans le monde de la programmation!"
+console.log(words1);
