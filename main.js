d57d39d3c4c517073215bd76e3b983c9fa1ab31f
anch365 <mchindaanchoura@gmail.com>
2026-03-24 10:07:17 +0100

    Exercice 2: Addition de deux nombres

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ console.log(words);
 
 let words1 = "Bienvenue dans le monde de la programmation!"
 console.log(words1);
+
+///// EXERCICE 2: ADDITION DE DEUX NOMBRES ///////
+
+let nombre1 = 5;
+let nombre2 = 10;
+
+console.log(nombre1+nombre2);
+
